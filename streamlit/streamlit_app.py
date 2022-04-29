13b035b068945d1009756f0d2b1c05b1fea3f3dc
John Hall <johngradyh@gmail.com>
2022-04-28 23:36:09 -0400

    removed extra app

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -6,21 +6,25 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import altair as alt
 from vega_datasets import data
 from streamlit_vega_lite import altair_component
 
 
+
 @st.cache
 def load_yelp_data():
     yelp_df = pd.read_csv('yelp_cleaned.csv')
+    #To Remove Unnamed Columns
+    yelp_df.loc[:, ~yelp_df.columns.str.contains('^Unnamed')]
     # yelp_df.set_index("county", drop=True,inplace=True)
     return yelp_df
 
 
 def load_pop_data():
     pop_df = pd.read_csv('population_density_data.csv')
+    #To Remove Unnamed Columns
+    pop_df.drop(pop_df.columns[pop_df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
     # pop_df.set_index("county",drop=True, inplace=True)
     return pop_df
 
@@ -50,18 +54,22 @@ with row2:
 # SHOWS SIDEBAR FUNCTIONS
 # show the data in a table
 if st.sidebar.checkbox('Show Yelp Dataset'):
+    st.subheader('Yelp Dataset')
     st.write(yelp_df)
 if st.sidebar.checkbox('Show Population Density Dataset'):
+    st.subheader('Population Density Dataset')
     st.write(pop_df)
-
 
 # show charts
 if st.sidebar.checkbox('Compare Ratings by Pop. Type in the US'):
+    st.subheader('Ratings by Population Type (US)')
     c = alt.Chart(yelp_df).mark_bar().encode(
         x='chain:N',
         y='count(stars):Q',
         color='chain:N',
         column='stars:Q'
+    ).configure_mark(
+        color='white'
     )
 
     st.altair_chart(c)
@@ -69,8 +77,22 @@ if st.sidebar.checkbox('Compare Ratings by Pop. Type in the US'):
     st.write('Local Average: ' + str(locals_df['stars'].mean()) + ' ★')
 
 if st.sidebar.checkbox('Compare Ratings by Pop. Type by State'):
-    rateState_df = yelp_df['stars'].value_counts()
-    st.bar_chart(data=rateState_df)
+    st.subheader('Ratings by Population Type (State)')
+    # state_abv =['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+    states = yelp_df['state'].unique()
+    option = st.selectbox('Select a state', states)
+    by_State = alt.Chart(yelp_df).mark_bar().encode(
+        x='chain:N',
+        y='count(stars):Q',
+        color='chain:N',
+        column='stars:Q'
+    ).configure_mark(
+        color='white'   
+    )
+    
+    st.altair_chart(by_State)
+    st.write('Chain Average: ' + str(chains_df['stars'].mean()) + ' ★')
+    st.write('Local Average: ' + str(locals_df['stars'].mean()) + ' ★')
 
 if st.sidebar.checkbox('View Chloropleth Map'):
     counties = alt.topo_feature(data.us_10m.url, 'counties')
@@ -97,16 +119,28 @@ if st.sidebar.checkbox('View Chloropleth Map'):
 
     r = event_dict.get('x')
 
-    st.write(event_dict)
-    if r:
-        print('test', r)
-        st.write(r)
+    # st.write(event_dict)
+    # if r:
+    #     print('test', r)
+    #     st.write(r)
 
     st.altair_chart(ch_map)
     
 
 
 if st.sidebar.checkbox('View Scatterplot of Pop. Density Ratings'):
-    fig, ax = plt.subplots()
-    pop_df.loc[pop_df[2]:pop_df[11]].plot(ax=ax)
-    st.write(fig)
+    #fig, ax = plt.subplots()
+    #pop_df.loc[pop_df[2]:pop_df[11]].plot(ax=ax)
+    #st.write(fig)
+    
+    #Creating a scatterplot for DENSITY(N) vs Stars
+    result_df = pd.concat([yelp_df, pop_df], axis=1)
+    result_df.plot.scatter(x='DENSITY(N)',y='stars')
+
+    # Creating a scatterplot for city vs stars 
+    yelp_df.plot.scatter(x='city',y='stars')
+
+    # Creating a scatterplot for city vs stars 
+    yelp_df.plot.scatter(x='county',y='stars')
+
+    
